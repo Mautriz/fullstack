cb8ee0de5853320c74834f13845f5d1016ee2fa1
Mauro Insacco <mauro.insacco@gmail.com>
2019-06-08 14:36:51 +0200

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}, your user has registered!`
+  })
+})
+
+app.listen(process.env.PORT || 8081)
